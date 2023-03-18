cca273d62c5971c9f8e14047935452a04be3103d
diegofigs <12281609+diegofigs@users.noreply.github.com>
2023-03-18 19:08:19 -0400

    feat: enable vercel edge runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/edge";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true,
   },
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+  }),
 });
